75c0100b3f70e4d6eb367b195169d2d220954342
TsarProgrammerII <mirandacm@g.cofc.edu>
2023-09-26 22:35:43 -0400

    modified:   TP D4/User.java

---
+++ TP D4/System.java
@@ -0,0 +1,67 @@
+/**
+ * The system class contains methods pertaining the basic functions of the ticketing system itself
+ * This may evolve into simply being the "Main" class, or we may keep the "Main" class as a separate entity
+ * 
+ * @Version 1.0 September 25, 2023
+ */
+
+public class System {
+
+    public System()
+    {
+        //Do we need a constructor for this class?
+    }
+
+    /**
+     * Once the user has confirmed their purchase, the system will make the necessary operations to transfer the required funds from the user's account into the system.
+     * This process will only be staged, not actually performed.
+     * However, this method will be responsible for calling the confirmation() and update() methods
+     * @return boolean indicating whether or not the transaction was successful
+     */
+    private boolean completeTransaction(String cardNumber)
+    {
+        if (confirmation())
+        {
+            //perform the payment operation, then:
+            if (update())
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Once payment method is input, this method will prompt the user to confirm their purchase before finalizing it.
+     * May be merged into another method/class implementation
+     * @return boolean to indicate User confirmation
+     */
+    private boolean confirmation()
+    {
+        if (true)   //The user confirms their purchase
+        {
+            return true;
+        }
+        return false; //The user denies their purchase
+    }
+
+    /**
+     * Once the transaction has been confirmed successful, the system will update the ticket inventory in the database to reflect the new number of tickets.
+     * This method may be broadened in scope to be usable by multiple classes for Database updates. Further testing required.
+     * @return boolean indicating whether the update was successful or not
+     */
+    private boolean update()
+    {
+        if (true) //complete
+        {
+            return true;
+        }
+        else //if not complete
+        {
+            return false;
+        }
+    }
+
+    
+}
--- TP D4/User.java
+++ TP D4/User.java
@@ -67,38 +67,6 @@ public class User
     }
 
     /**
-     * Once payment method is input, this method will prompt the user to confirm their purchase before finalizing it.
-     * May be merged into another method/class implementation
-     * @return boolean to indicate User confirmation
-     */
-    private boolean confirmation()
-    {
-        if (true)   //The user confirms their purchase
-        {
-            return true;
-        }
-        return false; //The user denies their purchase
-    }
-
-    /**
-     * Once the user has confirmed their purchase, the system will make the necessary operations to transfer the required funds from the user's account into the system.
-     * This process will only be staged, not actually performed.
-     * @return boolean indicating whether or not the transaction was successful
-     */
-    private boolean completeTransaction()
-    {
-        //Will complete the transaction (whatever that looks like)
-        if (true) //complete
-        {
-            return true;
-        }
-        else //if failed to complete
-        {
-            return false; 
-        }
-    }
-
-    /**
      * If the user has indicated that they would like to purchase a ticket (by pressing the "purchase ticket" button in the UI) they will be prompted for their credit card information.
      * The user will have the option of inputting a card manually, or using one that they registered with their account.
      * @return a card number, either one manually entered, or one already in the system.
@@ -110,23 +78,6 @@ public class User
             return "User Card Number";
         }
         return this.cardNumber; //User uses card info already stored in account
-    }
-
-    /**
-     * Once the transaction has been confirmed successful, the system will update the ticket inventory in the database to reflect the new number of tickets.
-     * This method may be broadened in scope to be usable by multiple classes for Database updates. Further testing required.
-     * @return boolean indicating whether the update was successful or not
-     */
-    private boolean update()
-    {
-        if (true) //complete
-        {
-            return true;
-        }
-        else //if not complete
-        {
-            return false;
-        }
     }
 
     //More methods likely to come in future iterations...
