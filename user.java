57602378c6fa64bdb5dc4eb4ee26a19382314390
lueckenea <143139418+lueckenea@users.noreply.github.com>
2023-11-16 15:38:52 -0500

    Create user.java

---
+++ user.java
@@ -0,0 +1,35 @@
+public class User {
+   private String username;
+   private String password;
+   private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    private payment(){
+
+    }
+    private login(){
+
+    }
+    private createAcct(){
+
+    }
+    private playName(){
+
+    }
+    private logOut(){
+
+    }
+    private deleteAcct(){
+
+    }
+    private reportBug(){
+
+    }
+    private selectEvent(){
+
+    }
+}
